6d2bce3a4bc0aaf2445696deccc6ac172c812d68
dragon-friend <aliceaccounts@protonmail.com>
2023-05-05 16:34:31 -0400

    learned about sum that shits pretty great

--- letterboxed.py
+++ letterboxed.py
@@ -1,26 +1,21 @@
 #!/bin/python3
 
-from functools import reduce
-
-
 def pairup(lst): return [a + b for a in lst for b in lst]
-def concat(a, b): return a + b
 
 
 wordlist = "o3000.txt"
 
-triples = [['e', 'i', 'o'],
-           ['m', 'c', 't'],
-           ['l', 'a', 'h'],
-           ['p', 'y', 'u']]
+triples = [['a', 'b', 'c'],
+           ['d', 'e', 'f'],
+           ['g', 'h', 'i'],
+           ['j', 'k', 'l']]
 
-validletters = set(reduce(concat, triples, []))
+validletters = set(sum(triples, []))
 
 solutions = [solution for solution in pairup(
     [word for word in open(wordlist, "r").read().split()
      if set(word) <= validletters
-     and all([pair not in word for pair in
-              reduce(concat, map(pairup, triples), [])])])
+     and all([pair not in word for pair in sum(map(pairup, triples), [])])])
              if set(solution) == validletters
              and any(a == b for a, b in zip(solution, solution[1:]))]
 
